44aace14e8777414fe8439dd9c01adee4a49a24b
Bruna <brunaborges7@gmail.com>
2024-01-15 20:12:15 -0300

    update forms

--- src/pages/ClassesForm.jsx
+++ src/pages/ClassesForm.jsx
@@ -1,13 +1,16 @@
+import { TEACHERS } from "../data/teachers";
 import { useState } from "react";
 import { CLASSES } from "../data/classes";
-import styled from "styled-components";
-import { TEACHERS } from "../data/teachers";
+import Button from "../ui/Button";
+import Form from "../ui/Form";
+import Select from "../ui/Select";
+import PageHeader from "../ui/PageHeader";
 
 function ClassesForm() {
   const [local, setLocal] = useState("");
   const [days, setDays] = useState("");
   const [hour, setHour] = useState("");
-  const [course, setCourse] = useState("");
+  const [stack, setStack] = useState("");
   const [teacher, setTeacher] = useState("");
 
   function newClass(e) {
@@ -20,6 +23,7 @@ function ClassesForm() {
       hour: +hour,
       stack: +stack,
       teacher,
+      students: [],
     };
     CLASSES.push(data);
     setLocal("");
@@ -30,52 +34,70 @@ function ClassesForm() {
   }
 
   return (
-  <form>
-    <div>CADASTRAR TURMA</div>
-  <select
-        onChange={(e) => setLocal(e.target.value)}
-        name="local"
-        id="local"
-        value={local}
-      >
-        <option value="">--- INFORME O LOCAL</option>
-        <option value="SOFTEX">SOFTEX</option>
-        <option value="UFPB">UFPB</option>
-        <option value="UFRN">UFRN</option>
-    </select>
-    <select
-        onChange={(e) => setDays(e.target.value)}
-        name="days"
-        id="days"
-        value={days}
-      >
-        <option value="">--- INFORME OS DIAS</option>
-        <option value="1">SEG - QUA</option>
-        <option value="2">TER - QUI</option>
-      </select>
-      <select 
-        onChange={(e) => setHour(e.target.value)}
-        name="hour"
-        id="hour"
-        value={hour}
-      >
-        <option value="">--- INFORME O HORÁRIO</option>
-        <option value="1">08:30 as 11:30</option>
-        <option value="2">14:00 as 17:00</option>
-        <option value="3">18:30 as 21:30</option>
-      </select>
-      <select
-        onChange={(e) => setCourse(e.target.value)}
-        name="course"
-        id="course"
-        value={course}
-      >
-        <option value="">--- INFORME O CURSO</option>
-        <option value="1">BACK-END</option>
-        <option value="2">FRONT-END</option>
-        </select>
-        <button onClick={newClass}>CADASTRAR TURMA</button>
-    </form>)
+    <>
+      <PageHeader>
+        <div>CADASTRAR TURMA</div>
+      </PageHeader>
+      <Form>
+        <Select
+          onChange={(e) => setLocal(e.target.value)}
+          name="local"
+          id="local"
+          value={local}
+        >
+          <option value="">--- INFORME O LOCAL</option>
+          <option value="SOFTEX">SOFTEX</option>
+          <option value="UFPB">UFPB</option>
+          <option value="UFRN">UFRN</option>
+        </Select>
+        <Select
+          onChange={(e) => setDays(e.target.value)}
+          name="days"
+          id="days"
+          value={days}
+        >
+          <option value="">--- INFORME OS DIAS</option>
+          <option value="1">SEG - QUA</option>
+          <option value="2">TER - QUI</option>
+        </Select>
+        <Select
+          onChange={(e) => setHour(e.target.value)}
+          name="hour"
+          id="hour"
+          value={hour}
+        >
+          <option value="">--- INFORME O HORÁRIO</option>
+          <option value="1">08:30 as 11:30</option>
+          <option value="2">14:00 as 17:00</option>
+          <option value="3">18:30 as 21:30</option>
+        </Select>
+        <Select
+          onChange={(e) => setStack(e.target.value)}
+          name="stack"
+          id="stack"
+          value={stack}
+        >
+          <option value="">--- INFORME A TRILHA</option>
+          <option value="1">BACK-END</option>
+          <option value="2">FRONT-END</option>
+        </Select>
+        <Select
+          onChange={(e) => setTeacher(e.target.value)}
+          name="teacher"
+          id="teacher"
+          value={teacher}
+        >
+          <option value="">--- INFORME O PROFESSOR</option>
+          {TEACHERS.map((t) => (
+            <option key={t.name} value={t.name}>
+              {t.name}
+            </option>
+          ))}
+        </Select>
+        <Button onClick={newClass}>CADASTRAR TURMA</Button>
+      </Form>
+    </>
+  );
 }
 
 export default ClassesForm;
